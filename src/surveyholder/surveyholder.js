5616cd0b5595044149006216d86f87200efd0d2f
gautamz07 <gautamz07@gmail.com>
2018-04-12 05:37:12 +0530

    Transition between components added.
    Transition between components added.

--- src/surveyholder/surveyholder.js
+++ src/surveyholder/surveyholder.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
-// import classes from './surveyholder.css';
-import welcome from './../components/welcome/welcome';
+import './surveyholder.css';
+import Welcome from './../components/welcome/welcome';
 import Generalinfo from './../components/generalinfo/generalinfo';
 import Preferences from './../components/preferences/preferences';
 import Navigation from './../UI/navigation/navigation';
-import { Route , BrowserRouter } from 'react-router-dom'; 
+import { Route , BrowserRouter , Switch } from 'react-router-dom'; 
+import { CSSTransition , TransitionGroup } from 'react-transition-group';
 
 
 class Surveyholder extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          <Navigation />
-          <Route path="/" exact component={ welcome } />  
-          <Route path="/generalinfo" exact component={ Generalinfo } />
-          <Route path="/preferences" exact component={ Preferences } />
-        </div>
+        <Route render={ ({ location }) => (
+          <div className="App">
+            <Navigation />
+            <TransitionGroup>
+              <CSSTransition key={location.key}  timeout={3000} classNames="fade">
+                <Switch location={ location }>
+                  <Route path="/" exact component={ Welcome } />  
+                  <Route path="/generalinfo" exact component={ Generalinfo } />
+                  <Route path="/preferences" exact component={ Preferences } />
+                </Switch>  
+              </CSSTransition>    
+            </TransitionGroup>          
+          </div>
+          )} />
       </BrowserRouter>
     );
   }
